30908c3f4877e86c72e14fa831a2eadddb8ed7ba
maksymmoskvychev <maksym.moskvychev@emesa.nl>
2018-05-03 23:05:58 +0200

    Allow multiple modules per bucket

--- aws_role.tf
+++ aws_role.tf
@@ -1,10 +1,10 @@
 resource "aws_iam_role" "iam_img_resize_role" {
-  name = "iam_img_resize_${md5(var.search_bucket)}_tf"
+  name = "iam_img_resize_${substr(md5(var.search_bucket), 0, 5)}_${substr(md5(var.config), 0, 5)}_tf"
   assume_role_policy = "${file("${path.module}/policy/lambda_role.json")}"
 }
 
 resource "aws_iam_policy" "iam_img_resize_policy" {
-  name = "iam_img_resize_${md5(var.search_bucket)}_tf"
+  name = "iam_img_resize_${substr(md5(var.search_bucket), 0, 5)}_${substr(md5(var.config), 0, 5)}_tf"
   policy = "${replace(file("${path.module}/policy/lambda_s3_policy.json"),"S3-BUCKET-NAME","${var.search_bucket}")}"
 }
 
